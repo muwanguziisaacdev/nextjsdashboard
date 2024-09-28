1e0edca54fd997203691d4e3ab8159742fc865f4
muwanguziisaacdev <muwanguziisaacuganda@gmail.com>
2024-09-28 03:05:19 +0300

    still fixing the paths

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -8,6 +8,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 
+
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
     <form>
